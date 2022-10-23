6dee075c52391a372aac59d9905a3858f4e55a31
soulteary <soulteary@gmail.com>
2022-10-24 00:03:43 +0800

    feat: auto prepare data directory

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 	"gateway/internal/config"
 	"gateway/internal/web"
+	"os"
+	"path/filepath"
 )
 
 const (
 	DEFAULT_PORT = "3000"
 )
+
+func init() {
+	prepare := filepath.Join(".", "data")
+	os.MkdirAll(prepare, os.ModePerm)
+}
 
 func main() {
 	var appPort = config.SetDataFromEnv("GATE_APP_PORT", DEFAULT_PORT)
